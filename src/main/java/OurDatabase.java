84c8e6012524d6c7a2b2ed2c75370be9433a4f9e
PotatoB666 <1363964141@qq.com>
2022-03-10 16:43:48 +0800

    添加封面

--- src/main/java/OurDatabase.java
+++ src/main/java/OurDatabase.java
@@ -1,7 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Logger;
 
 public class OurDatabase {
     private static OurDatabase db = null;
+    private static Class driver = null;
+    private static Connection conn = null;
     /**
      * 获取数据库
      */
@@ -11,7 +17,7 @@ public class OurDatabase {
                 if (db == null) {
                     try {
                         db = new OurDatabase();
-                    }catch (ClassNotFoundException e){
+                    }catch (ClassNotFoundException|SQLException e){
                         System.out.println("数据库连接错误");
                     }
                 }
@@ -20,8 +26,27 @@ public class OurDatabase {
         return db;
     }
 
-    private OurDatabase() throws ClassNotFoundException {
-        Class driver = Class.forName("com.mysql.jdbc.Driver");
+    private OurDatabase() throws ClassNotFoundException, SQLException {
+        driver = Class.forName("com.mysql.jdbc.Driver");
+        conn = DriverManager.getConnection("jdbc:mysql://1.116.21.195:3306/library", "jsp", "1008611");
+    }
 
+    /**
+     * 获取新的Statement
+     * @return 新的Statement
+     */
+    public Statement getNewStatement(){
+        Statement s = null;
+        try {
+            s = conn.createStatement();
+        } catch (SQLException throwables) {
+            System.out.println("SQL错误");
+            throwables.printStackTrace();
+            db = null;
+            driver = null;
+            conn = null;
+
+        }
+        return s;
     }
 }
