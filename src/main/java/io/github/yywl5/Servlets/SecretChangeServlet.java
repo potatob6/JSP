34b4718c5c7ac9898e18ff7beb238ba94b1b8257
yywl5 <1419453530@qq.com>
2022-03-17 19:58:38 +0800

    test

--- src/main/java/io/github/yywl5/Servlets/SecretChangeServlet.java
+++ src/main/java/io/github/yywl5/Servlets/SecretChangeServlet.java
@@ -44,7 +44,7 @@ public class SecretChangeServlet extends HttpServlet {
         }
         response.setContentType("text ml;charset=gb2312");
         try{
-            OurDatabase editpasswordseek = OurDatabase.getDataBase();
+            UserBean userBean = new UserBean();
             if (username==null || password ==null || password2 == null){
                 response.sendRedirect("reSecretChange.jsp?errmsg=inforamtion error");
             }
