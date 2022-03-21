8d1f1085f9d22d5900a67934a8b072f329516298
Y0looo <63961776+Y0looo@users.noreply.github.com>
2022-03-21 13:54:36 +0800

    增加查询所有用户和借阅信息函数

--- src/main/java/io/github/potatob6/Models/OurDatabase.java
+++ src/main/java/io/github/potatob6/Models/OurDatabase.java
@@ -682,4 +682,40 @@ public class OurDatabase {
         formatter.format("%04d-%02d-%02d", nowDay.get(Calendar.YEAR), nowDay.get(Calendar.MONTH)+1, nowDay.get(Calendar.DAY_OF_MONTH));
         return Date.valueOf(formatter.out().toString());
     }
+
+    /**
+     * 查询所有用户信息
+     * @return
+     */
+    public ArrayList<Object> queryAllUsers(){
+        Connection connection = null;
+        try {
+            connection = this.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from Users;");
+            ResultSet result = preparedStatement.executeQuery();
+            ArrayList<Object> allUsers = fullSetupMultiByQuery(result, UserBean.class);
+            return allUsers;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 查询所有借阅信息
+     * @return
+     */
+    public ArrayList<Object> queryAllBorrows(){
+        Connection connection = null;
+        try {
+            connection = this.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from Borrow;");
+            ResultSet result = preparedStatement.executeQuery();
+            ArrayList<Object> allBorrows = fullSetupMultiByQuery(result, BorrowBean.class);
+            return allBorrows;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return null;
+        }
+    }
 }
